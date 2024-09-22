365654224b524cd0ba6b75d80c35e9e2609df3b1
atharianr <atharianrahmadani@gmail.com>
2024-09-22 19:28:29 +0700

    Initial Commit

---
+++ PlayON/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayON
+//
+//  Created by Atharian Rahmadani on 22/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
