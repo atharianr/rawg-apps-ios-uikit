e95e9942ef505c15b4c2b9d3f11d76de911dc856
atharianr <atharianrahmadani@gmail.com>
2024-09-26 20:09:23 +0700

    Implement profile and change app name

---
+++ PlayON/View/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  PlayON
+//
+//  Created by Atharian Rahmadani on 26/09/24.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    func setupView() {
+        profileImageView.layer.borderWidth = 1
+        profileImageView.layer.masksToBounds = false
+        profileImageView.layer.borderColor = UIColor.colorPrimary.cgColor
+        profileImageView.layer.cornerRadius = 16
+        profileImageView.clipsToBounds = true
+    }
+}
